48fde0bcf141782768fabdb96167de7676300d59
Supriya Kadam <kadamsupriya900@gmail.com>
2024-02-22 10:33:16 +0100

    rails-yelp-mvp exercise

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  #   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  #   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  #   get "up" => "rails/health#show", as: :rails_health_check
+  #   resources :restaurants
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  #   # Defines the root path route ("/")
+  #   # root "posts#index"
+
+  # Restaurants
+  get 'restaurants', to: 'restaurants#index'
+  get '/restaurants/new', to: 'restaurants#new'
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  post 'restaurants', to: 'restaurants#create'
+
+  # Review
+  get 'restaurants/:id/reviews/new', to: 'reviews#new'
+  post 'restaurants/:id/reviews', to: 'reviews#create'
+  delete 'reviews/:id', to: 'reviews#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+honghong = {name: "Honghong", address: "Gieselastrasse, Munich", phone_number:"1234-5678" ,category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number:"99990000", category: "italian"}
+dishoom =  {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number:"70707070", category: "japanese"}
+swaad = {name: "Swaad", address: "Hauptbahnhof, Munich", phone_number:"9876543", category: "belgian"}
+french_corner = {name: "French Corner", address: "18 Boundary St, France E2 7JE", phone_number:"345345", category: "french"}
+
+[honghong, pizza_east, dishoom, swaad, french_corner].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
